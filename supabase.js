2bdaaa2e42c88fdb0b4dadd172c8ebb2a6791561
Kuri <enrico.feldmann@optimal-systems.de>
2026-08-15 04:24:02 +0200

    Update website

---
+++ supabase.js
@@ -0,0 +1,7 @@
+const SUPABASE_URL = "https://bgibljpszqsmntvtectm.supabase.co";
+const SUPABASE_KEY = "sb_publishable_0SHA4sy9w-tSU11xts_t6A_L30fLnR8";
+
+const supabaseClient = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+);
